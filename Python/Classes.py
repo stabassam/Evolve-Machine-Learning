c92f4534fcaee73e351b92dc02bbf62d5527a352
Shahraiz Tabassam <shahraiz.t@gmail.com>
2018-02-08 20:32:00 +0500

    Learnt about creating Object Classes and methods in Python
    Learnt about creating Object Classes and methods in Python to solve Matrices.

---
+++ Python/Classes.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  8 20:08:38 2018
+
+@author: stabassam
+"""
+
+class Vector(object):
+    
+    #Constructor
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+        except ValueError:
+            raise ValueError('The coordinates myst be non-empty')
+    #Addition    
+    def plus(self,v):
+        new_coordinates = [ x + y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+    
+    #Substraction
+    def minus(self,v):
+        new_coordinates = [ x - y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    #Scalar Multiplication
+    def times_scalar(self,c):
+        new_coordinates = [ c * x for x in self.coordinates]
+        return Vector(new_coordinates)
+
+    #String Representation Format
+    def __str__(self):
+        return 'Vector : {0}' . format(self.coordinates)
+    
+    #Comparator
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+my_vector = Vector ([1,2,3,4])
+print(my_vector)
+
+my_vector2 = Vector ([1,2,3,4])
+print(my_vector == my_vector2)
+
+v = Vector([8.218, -9.341])
+w = Vector([1.129, 2.111])
+
+print(v.plus(w))            
